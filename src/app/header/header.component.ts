419277d2b89b2bbf2878a4ac1f32ce152a5f60c8
chiangs <stephen.e.chiang@gmail.com>
2017-11-07 17:45:39 +0100

    added routerModule, updated index with base href, header nav working with router

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, HostBinding, } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +8,24 @@ import { Component, OnInit, HostBinding, EventEmitter, Output } from '@angular/c
             CookR
         </div>
         <div class="right menu">
-            <a class="item" href="#" (click)="selectComponent('recipes')"><i class="ui book icon"></i>Recipes</a>
-            <a class="item" href="#" (click)="selectComponent('shopping-list')"><i class="ui add shopping basket icon"></i>Shopping List</a>
+            <a class="item" [routerLink]="['/recipes']"
+                            routerLinkActive="active"
+                            [routerLinkActiveOptions]="{exact: true}">
+                                <i class="ui book icon"></i>
+                                Recipes
+            </a>
+            <a class="item" [routerLink]="['shopping-list']"
+                            routerLinkActive="active"
+                            [routerLinkActiveOptions]="{exact: true}">
+                                <i class="ui add shopping basket icon"></i>
+                                Shopping List
+            </a>
             <div class="ui item pointing dropdown" appDropdown #dropRef>
                 Manage
                 <i class="dropdown icon"></i>
                 <div class="menu transition" [class.visible]="dropRef.classList.contains('visible')">
-                    <a class="item" href="#"><i class="ui star icon"></i>Save Data</a>
-                    <a class="item" href="#"><i class="ui check circle icon"></i>Fetch Data</a>
+                    <a class="item"><i class="ui star icon"></i>Save Data</a>
+                    <a class="item"><i class="ui check circle icon"></i>Fetch Data</a>
                 </div>
             </div>
         </div>
@@ -24,17 +34,18 @@ import { Component, OnInit, HostBinding, EventEmitter, Output } from '@angular/c
 })
 export class HeaderComponent implements OnInit {
   @HostBinding('attr.class') containerClass = 'ui top fixed icon compact menu';
-  @Output() selection: EventEmitter <string>;
+  // @Output() selection: EventEmitter <string>; no longer used due to router
 
   constructor() {
-      this.selection = new EventEmitter();
+    //   this.selection = new EventEmitter(); no longer used due to router
   }
 
   ngOnInit() {
   }
 
-  selectComponent(feature: string) {
-      this.selection.emit(feature);
-  }
+  // selectComponent(feature: string) {
+  //    no longer used due to routing
+  //     this.selection.emit(feature);
+  // }
 
 }
